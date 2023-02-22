78ac67e0c37b5523e31b8d30d80a15e7ff5eda1c
Hazza158 <99371032+Hazza158@users.noreply.github.com>
2023-02-22 14:49:55 +1300

    Add files via upload

---
+++ 01_yes_noV1.py
@@ -0,0 +1,15 @@
+#functions go here
+
+
+
+
+# main routine goes here
+want_instructions = input("do you want to read the instructions?")
+
+if want_instructions == "yes":
+    print("instructions go here")
+elif want_instructions == "no":
+    pass
+else:
+    print("please answer yes / no")
+
