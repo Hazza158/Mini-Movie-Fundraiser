fd39c6d2717678f44184be9efd60bcef70c6d1fb
Hazza158 <99371032+Hazza158@users.noreply.github.com>
2023-02-24 12:17:10 +1300

    Add files via upload

---
+++ 02_looping.py
@@ -0,0 +1,23 @@
+# main routine goes here
+
+
+
+# set max num of tickets below
+MAX_TICKETS = 3
+
+
+# loop to sell tickets
+tickets_sold = 0
+while tickets_sold < MAX_TICKETS:
+    name = input("Please enter your name or 'xxx' to quit: ")
+
+    if name == 'xxx':
+        break
+
+    tickets_sold += 1
+
+# output number of tickets sold
+if tickets_sold == MAX_TICKETS:
+    print("congrats you have sold all the tickets")
+else:
+    print("you have sold {} ticket/s. there is {} ticket/s remaining").format(tickets_sold, MAX_TICKETS - tickets_sold)
