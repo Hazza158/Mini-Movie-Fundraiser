5a28aa21a84f4cc7535a1ca07fb7dc9044281e99
Hazza158 <99371032+Hazza158@users.noreply.github.com>
2023-02-23 13:28:21 +1300

    Add files via upload

--- 01_yes_noV1.py
+++ 01_yes_noV1.py
@@ -4,12 +4,15 @@
 
 
 # main routine goes here
+# checks for yes / no, returns "yes" / "no"
 want_instructions = input("do you want to read the instructions?")
 
-if want_instructions == "yes":
+if want_instructions == "yes" or want_instructions == "y":
     print("instructions go here")
-elif want_instructions == "no":
+elif want_instructions == "no" or want_instructions == "n":
     pass
 else:
     print("please answer yes / no")
+
+print("we are done")
 
---
+++ 01_yes_noV2.py
@@ -0,0 +1,25 @@
+# functions go here
+def yes_no(question):
+
+    while True:
+        response = input(question).lower()
+
+        if response == "yes" or response == "y":
+            return "yes"
+
+        elif response == "no" or response == "n":
+            return "no"
+
+        else:
+            print("please enter yes or no")
+
+# main routine goes here
+# checks for yes / no, returns "yes" / "no"
+
+want_instructions = yes_no("do you want to read the instructions?")
+
+if want_instructions == "yes" or want_instructions == "y":
+    print("instructions go here")
+
+print("program continues")
+print()
