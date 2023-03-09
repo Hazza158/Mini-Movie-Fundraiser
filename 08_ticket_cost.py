3371aa9f245d5a052e4c0d6fe07f50714e929bc9
Hazza158 <99371032+Hazza158@users.noreply.github.com>
2023-03-09 13:28:59 +1300

    Add files via upload

--- 08_ticket_cost.py
+++ 08_ticket_cost.py
@@ -1,5 +1,9 @@
 import pandas
 
+# currency formatting function
+def currency(x):
+    return "${:.2f}".format(x)
+
 # dictionaries to hold ticket details
 all_names = ["a", "b", "c", "d", "e"]
 all_ticket_costs = [7.50, 7.50, 10.50, 10.50, 6.50]
@@ -19,6 +23,15 @@ mini_movie_frame['Total'] = mini_movie_dict['surcharge'] + mini_movie_frame['Tic
 # Calculate the profit for each ticket
 mini_movie_frame['Profit'] = mini_movie_frame['Ticket Price'] - 5
 
+# calculate ticket and profit totals
+total = mini_movie_frame["Total"].sum()
+profit = mini_movie_frame["Profit"].sum()
+
+# output table with ticket data
 print(mini_movie_frame)
+
+# output total ticket sales and profit
+print("Total Ticket Sales: ${:.2f}".format(total))
+print("Total Profit : ${:.2f}".format(profit))
 
 
